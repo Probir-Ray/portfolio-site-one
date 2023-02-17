028a1e3479d820ba9b3cc60f0e81fa0558b73796
Probir-Ray <somironroy.need@gmail.com>
2023-02-17 15:51:14 +0600

    update

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,3 @@
-
-        (function() {
-            emailjs.init("user_tAx2evroSRIx7Qek2cMed");
-        })();
-
-        window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                this.contact_number.value = Math.random() * 100000 | 0;
-                emailjs.sendForm('service_lfsjaxd', 'template_xeex2ho', '#contact-form', 'user_tAx2evroSRIx7Qek2cMed')
-                    .then(function() {
-                        alert('SUCCESS!');
-                        location.reload();
-                    }, function(error) {
-                        alert('FAILED...', error);
-                    });
-            });
-        }
-
-        
-    // AOS animation js
-        AOS.init();
-
     // Testimonial
         $(".hover").mouseleave(
             function() {
